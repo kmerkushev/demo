b433cefd8430570bb893ffef56d1c62189f436fe
kmerkushev <regesc@yandex.ru>
2024-09-12 23:50:06 +0545

    BD

--- js/tg-toggle.js
+++
@@ -1,8 +0,0 @@
-const tgToggle = document.querySelector(`#socials-telegram-toggle`);
-const tgList = document.querySelector(`#socials-telegram-list`);
-
-const toggleLinks = () => {
-    tgList.classList.toggle(`opened`);
-}
-
-tgToggle.addEventListener(`click`, toggleLinks);
--- js/toggleLang.js
+++
@@ -1,8 +0,0 @@
-const langToggle = document.querySelector(`.lang-toggle`);
-const langList = document.querySelector(`#lang-list`);
-
-const toggleLangMenu = () => {
-    langList.classList.toggle(`opened`);
-}
-
-langToggle.addEventListener(`click`, toggleLangMenu);
